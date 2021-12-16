7ef8fd64a7180a32f8668771be8d6058b2199b7c
Pedro-18 <1055499770@qq.com>
2021-12-16 17:30:21 +0800

    路由学习第一天

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,17 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
+/*
 Route::get('/', function () {
-    return view('welcome');
+     return view('welcome');
+});
+ */
+/*
+Route::get('/',function(){
+    return "Hello,world!";
+});
+ */
+
+Route::post('/',function(){
+    return "这是POST请求";
 });
